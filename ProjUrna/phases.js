e4975ddd1ec4e2b406fac99a34512b4aa25f89e0
luciano.vtn <luciano.vtn120@gmail.com>
2021-06-19 13:49:12 -0300

    Up de controles da interface do HTML

---
+++ ProjUrna/phases.js
@@ -0,0 +1,66 @@
+let phases = [
+
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12345,
+                nome:   "Infinito e Alem",
+                partido:"JPD",
+                fotos: [
+                    {
+                        url: "12345.jpg", legenda: "Vereador" 
+                    }
+                ]
+            },
+            {
+                numero: 12001,
+                nome:   "Tranquila que Só",
+                partido:"PDA",
+                fotos: [
+                    {
+                        url: "12001.jpg", legenda: "Vereador"
+                    }
+                ]
+            },
+            {
+                numero: 12020,
+                nome:   "Atento e Observador",
+                partido:"EJO",
+                fotos: [
+                    {
+                        url: "12020.jpg", legenda: "Vereador"
+                    }
+                ]
+            },
+        ]
+    },    
+// "PREFEITO E VICE PREFEITO"
+            {
+                titulo: "PREFEITO",
+                numeros: 2,
+                candidatos: [
+                    {
+                        numero: 15,
+                        nome:   "Java para Todos",
+                        partido:"JPD",
+                        vice:"De olho",
+                        fotos: [
+                            {url: "15.jpg", legenda: "Prefeito"},
+                            {url: "11.jpg", legenda: "Vice-Prefeito", small: true}
+                        ]
+                    },
+                    {
+                        numero: 12,
+                        nome:   "Html é de Marcação",
+                        partido:"EJO",
+                        vice:"Esperando",
+                        fotos: [
+                            {url: "12.jpg", legenda: "Prefeito"},
+                            {url: "02.jpg", legenda: "Vice-Prefeito", small: true}
+                        ]
+                    },
+                ]
+            }
+];
---
+++ ProjUrna/sript.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector(".topScreenLeft-01 span");
+let cargo = document.querySelector(".topScreenLeft-02 span");
+let descricao = document.querySelector(".topScreenLeft-04");
+let screenBotton = document.querySelector(".bottonSreen");
+let image = document.querySelector(".topScreenRight");
+let numerosCandidatos = document.querySelector(".topScreenLeft-03");
